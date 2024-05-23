7886e0ffc3351f7d30e16b5f1c426aa7d05288bf
mdfaiskhan <mdfaiskhan@gmail.com>
2024-05-23 16:49:10 +0530

    first commit

---
+++ 02_dataTypes.js
@@ -0,0 +1,12 @@
+let Faisal = 1
+let khan = "Mr"
+
+//int 2 to power of 52
+//bigint
+//string-" "
+//boolean - true or false
+//null - nothing
+//undefined - no value but can be changed
+//symbol =>unique
+
+console.log(typeof null)
